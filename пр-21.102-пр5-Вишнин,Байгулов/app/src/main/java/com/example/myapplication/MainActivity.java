48e2e2b7b450aa7ea6759f6fc6f3bf0bebf8bc5e
DanilVishnin <126852995+DanilVishnin@users.noreply.github.com>
2023-03-10 13:58:28 +0700

    Add files via upload

---
+++ пр-21.102-пр5-Вишнин,Байгулов/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //commit 4
+        setContentView(R.layout.activity_main);
+        //commit_vetca_5
+    }
+}
